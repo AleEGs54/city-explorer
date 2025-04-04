81256872ab1b43d5ddd02b273d08d0b12edde547
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-04-03 18:10:26 -0700

    First Commit

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    "rules": {
+      "no-unused-vars": [
+        "warn",
+        {
+          "argsIgnorePattern": "res|next|^err"
+        }
+      ],
+      "no-console": "warn",
+      "no-param-reassign": [
+        "error",
+        {
+          "props": false
+        }
+      ],
+      "no-shadow": [
+        "error",
+        {
+          "hoist": "all",
+          "allow": ["resolve", "reject", "done", "next", "err", "error"]
+        }
+      ],
+      "arrow-body-style": [
+        "warn",
+        "as-needed"
+      ]
+    }
+  }
+]);
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/", // Define la carpeta raíz del proyecto
+
+  build: {
+    outDir: "../dist", // Carpeta de salida de los archivos compilados
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html")
+      },
+    },
+  },
+});
