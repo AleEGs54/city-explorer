e41ece9d349dcd97df03e3f8ce90d1987d321556
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-04-04 14:53:09 -0700

    Add additional entry points for map and place details pages in Vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ export default defineConfig({
     outDir: "../dist", // Carpeta de salida de los archivos compilados
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html")
+        main: resolve(__dirname, "src/index.html"),
+        map: resolve(__dirname, "src/mapPage/map.html"),
+        place: resolve(__dirname, "src/placeDetailsPage/placeDetails.html"),
       },
     },
   },
