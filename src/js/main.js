f4fe7ffadf86157f25cc6c05b07791b12cf7054f
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-04-04 14:47:11 -0700

    trying to test the build version

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,24 @@
-let hello = 'hello'
-document.querySelector('#app').innerHTML = hello
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+const app = document.querySelector('#app');
+const mapDiv = document.createElement('div');
+
+mapDiv.textContent = 'Hola Mundo'
+app.appendChild(mapDiv);
+
+async function buscarLugar(placeName) {
+    const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(placeName)}&key=${apiKey}`;
+  
+    try {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+  
+      const data = await response.json();
+      console.log('Resultado de Google Places API:', data);
+    } catch (error) {
+      console.error('Hubo un error al hacer la solicitud:', error);
+    }
+  }
+  
+  // Ejemplo de uso:
+  buscarLugar('Torre Eiffel');
