6299d0b5ae4fbd10a0c97b36cd48b19c060e6a08
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-04-08 18:07:43 -0700

    Refactor location retrieval to use an async function for better structure

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,15 @@ const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 
 const userLocation = new UserLocation();
-const location = await userLocation.getLocation()
 
-console.log(location);
+const location = async () => {
+
+  const locationData = await userLocation.getLocation()
+  console.log(locationData);
+}
+
+location();
+
 
 
 
